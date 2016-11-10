a6f4b8b1d4064368ce41734a65639c15a9c10b8b
Triptophan <antimilo@gmail.com>
2016-11-10 08:36:54 -0600

    Added a test projectile.

---
+++ Assets/Scripts/Combat/Projectiles/Rock.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Assets.Scripts.Enemies;
+
+namespace Assets.Scripts.Combat.Projectiles
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(SphereCollider))]
+    public class Rock : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            var enemy = collision.gameObject.GetComponent<Enemy>();
+
+            if (enemy == null) return;
+
+            Destroy(this);
+        }
+    }
+}
--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -5,8 +5,13 @@ namespace Assets.Scripts.Enemies
 {
     [RequireComponent(typeof(NavMeshAgent))]
     [RequireComponent(typeof(WanderBehavior))]
+    [RequireComponent(typeof(BoxCollider))]
     public class Enemy : MonoBehaviour
     {
-        public bool Active { get { return isActiveAndEnabled; } set { gameObject.SetActive(value); } }
+        public bool Active
+        {
+            get { return isActiveAndEnabled; }
+            set { gameObject.SetActive(value); }
+        }
     }
 }
