e03fd2850beaf7f11e601ffefe976ffe90469933
edrowley951 <edrowley@erinrowley.com>
2016-11-10 21:27:58 -0600

    Added some cool focus sphere stuff

--- Assets/Scripts/Enemies/AI/ChaseBehavior.cs
+++ Assets/Scripts/Enemies/AI/ChaseBehavior.cs
@@ -6,30 +6,40 @@ namespace Assets.Scripts.Enemies.AI
     {
         public bool isChasing = false;
         public float playerDetectionRadius = 20f;
+        public float playerFoundDetectionRadiusMultiplier = 1.5f;
         private NavMeshAgent agent;
         //private LevelManager levelManager;
         private bool hadPlayer = false;
         Vector3 initialPosition;
 
-        private void OnEnable()
+        private void OnStart()
         {
             agent = GetComponent<NavMeshAgent>();
             //Save off initial position
             initialPosition = transform.position;
         }
 
+        private void OnEnable()
+        {
+
+        }
+
         public void Update()
         {
-            var x = Physics.OverlapSphere(transform.position, playerDetectionRadius);
-            foreach (var z in x)
+            var collisions = Physics.OverlapSphere(transform.position, playerDetectionRadius);
+            
+            foreach (var collision in collisions)
             {
-               if(z.GetType() == typeof(CharacterController))
+               if(collision.GetType() == typeof(CharacterController))  //is the player being detected?
                 {
                     //We have player
                     isChasing = true;
                     hadPlayer = true;
+                    
+                    //enemy is focused now, increase the playerdetectionradius
+                    playerDetectionRadius = playerDetectionRadius * playerFoundDetectionRadiusMultiplier;
 
-                    var playerPosition = z.transform.position;
+                    var playerPosition = collision.transform.position;
                     agent.SetDestination(playerPosition);
                     break;
                 }
@@ -41,6 +51,8 @@ namespace Assets.Scripts.Enemies.AI
                     //We are no longer chasing the player so return to my original location
                     if (hadPlayer)
                     {
+                        //lost the enemy, reset playerdetectionradius
+                        playerDetectionRadius = playerDetectionRadius / playerFoundDetectionRadiusMultiplier;
                         hadPlayer = false;
                         agent.SetDestination(initialPosition);
                     }
