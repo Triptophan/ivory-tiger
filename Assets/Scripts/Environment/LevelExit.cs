2518cb77353eeb14a03b70926c354234c8a06d98
Kohler, Dean <Dean_Kohler@bradycorp.com>
2017-01-09 18:17:38 -0600

    Added a test rock for dungeon layout testing.

--- Assets/Scripts/Environment/LevelExit.cs
+++ Assets/Scripts/Environment/LevelExit.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
+using Assets.Scripts.StateMachine.States.GameFlowStates;
 
 namespace Assets.Scripts.Environment
 {
-    public class LevelExit : MonoBehaviour
-    {
+	public class LevelExit : MonoBehaviour
+	{
+		public GameManager GameManager;
 
-    }
+		void OnTriggerEnter(Collider other)
+		{
+			if (other.tag == "Player")
+			{
+				GameManager.StateMachine.ChangeGlobalState(GameRestartState.Instance);
+			}
+		}
+	}
 }
--- Assets/Scripts/Player/CombatController.cs
+++ Assets/Scripts/Player/CombatController.cs
@@ -4,81 +4,81 @@ using System;
 
 namespace Assets.Scripts.Player
 {
-    public class CombatController : MonoBehaviour
-    {
-        private GameObject _playerObject;
-        private Transform _viewTransform;
+	public class CombatController : MonoBehaviour
+	{
+		GameObject _playerObject;
+		Transform _viewTransform;
 
-        private PlayerHealth _playerHealth;
+		PlayerHealth _playerHealth;
 
-        public GameObject Projectile;
+		public GameObject Projectile;
 
-        public Camera PlayerView;
+		public Camera PlayerView;
 
-        public float PlayerHealthIndicatorFillAmount
-        {
-            get { return _playerHealth.HealthIndicatorFillAmount; }
-        }
+		public float PlayerHealthIndicatorFillAmount
+		{
+			get { return _playerHealth.HealthIndicatorFillAmount; }
+		}
 
-        public bool CanFire;
+		public bool CanFire;
 
-        public bool IsDead = false;
-        public Action OnDeath { get; set; }
+		public bool IsDead;
+		public Action OnDeath { get; set; }
 
-        public void Awake()
-        {
-            _playerObject = gameObject;
-            if (PlayerView != null) _viewTransform = PlayerView.transform;
+		public void Awake()
+		{
+			_playerObject = gameObject;
+			if (PlayerView != null) _viewTransform = PlayerView.transform;
 
-            _playerHealth = _playerObject.GetComponent<PlayerHealth>();
-        }
+			_playerHealth = _playerObject.GetComponent<PlayerHealth>();
+		}
 
-        public void Update()
-        {
-            GetInput();
-        }
+		public void Update()
+		{
+			GetInput();
+		}
 
-        public void ResetPlayer()
-        {
-            _playerHealth.ResetHealth();
-            IsDead = false;
-        }
+		public void ResetPlayer()
+		{
+			_playerHealth.ResetHealth();
+			IsDead = false;
+		}
 
-        private void GetInput()
-        {
-            if (CanFire && Input.GetButtonUp("Fire1"))
-            {
-                var projectileObject = (GameObject)Instantiate(Projectile, _viewTransform.TransformPoint(Vector3.forward - Vector3.up / 2f), _viewTransform.rotation);
+		void GetInput()
+		{
+			if (CanFire && Input.GetButtonUp("Fire1"))
+			{
+				var projectileObject = (GameObject)Instantiate(Projectile, _viewTransform.TransformPoint(Vector3.forward - Vector3.up / 2f), _viewTransform.rotation);
 
-                var projectileBehavior = projectileObject.GetComponent<Rock>();
-                projectileBehavior.OwnerTransform = _viewTransform;
-            }
-        }
+				var projectileBehavior = projectileObject.GetComponent<Rock>();
+				projectileBehavior.OwnerTransform = _viewTransform;
+			}
+		}
 
-        private Vector3 GetCameraCenter()
-        {
-            return new Vector3(Screen.width / 2, Screen.height / 2, PlayerView.nearClipPlane);
-        }
+		Vector3 GetCameraCenter()
+		{
+			return new Vector3(Screen.width / 2, Screen.height / 2, PlayerView.nearClipPlane);
+		}
 
-        private void OnCollisionEnter(Collision collision)
-        {
-            CheckEnemyCollision(collision.gameObject);
-        }
+		void OnCollisionEnter(Collision collision)
+		{
+			CheckEnemyCollision(collision.gameObject);
+		}
 
-        private void OnCollisionStay(Collision collision)
-        {
-            CheckEnemyCollision(collision.gameObject);
-        }
+		void OnCollisionStay(Collision collision)
+		{
+			CheckEnemyCollision(collision.gameObject);
+		}
 
-        private void CheckEnemyCollision(GameObject collidedObject)
-        {
-            if (collidedObject.tag == "Enemy")
-            {
-                _playerHealth.DoDamage(10f);
+		void CheckEnemyCollision(GameObject collidedObject)
+		{
+			if (collidedObject.tag == "Enemy")
+			{
+				_playerHealth.DoDamage(10f);
 
-                IsDead = _playerHealth.CurrentHealth <= 0;
-                if (IsDead) OnDeath();
-            }
-        }
-    }
+				IsDead = _playerHealth.CurrentHealth <= 0;
+				if (IsDead) OnDeath();
+			}
+		}
+	}
 }
